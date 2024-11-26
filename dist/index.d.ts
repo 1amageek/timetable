6563363cd5b96e43b9e677c8d949f600d2050e4c
1amageek <nori@stamp.team>
2024-11-27 02:50:38 +0900

    Update .gitignore, remove unused dist files, configure tsup

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { Timetable } from './timetable.js';
-export { Timeline } from './timeline.js';
-export { TimetableProvider, useTimetableContext } from './timetable-context.js';
-export { useTimetable } from './use-timetable.js';
-export { DateTimeRange, EdgeInsets, Identifiable, Position, TimetableOptions, TimetableProps } from './timetable-types.js';
-import 'react/jsx-runtime';
-import 'react';
-import 'luxon';
--- dist/timetable-header.d.ts
+++
@@ -1,9 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-import { TimetableOptions } from './timetable-types.js';
-import 'luxon';
-
-declare function TimetableHeader({ options }: {
-    options: TimetableOptions;
-}): react_jsx_runtime.JSX.Element;
-
-export { TimetableHeader };
